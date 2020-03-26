0fa377ac88679f04b840520cc2b33e09607110dc
rodrigo novais <roo.novais@gmail.com>
2020-03-26 17:00:34 -0300

    Change anchor for Link component

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,29 +1,26 @@
 import React from 'react'
 import { FiLogIn } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
 import logoImage from '../../assets/logo.svg'
 import heroesImage from '../../assets/heroes.png'
 
 import './styles.css'
 
-function Login() {
-    return (
-        <div className="login-container">
-            <section className="form">
-                <img src={logoImage} alt="Be The Hero" />
-                <form>
-                    <h1>Faça seu login</h1>
+export default () => (
+    <div className="login-container">
+        <section className="form">
+            <img src={logoImage} alt="Be The Hero" />
+            <form>
+                <h1>Faça seu login</h1>
 
-                    <input type="text" placeholder="Sua ID" />
-                    <button className="button" type="submit">Entrar</button>
+                <input type="text" placeholder="Sua ID" />
+                <button className="button" type="submit">Entrar</button>
 
-                    <a href="/register"><FiLogIn size={16} color="#E02041" /> Não tenho cadastro</a>
-                </form>
-            </section>
+                <Link className="back-link" to="/register"><FiLogIn size={16} color="#E02041" /> Não tenho cadastro</Link>
+            </form>
+        </section>
 
-            <img src={heroesImage} alt="Heroes"/>
-        </div>
-    )
-}
-
-export default Login
+        <img src={heroesImage} alt="Heroes"/>
+    </div>
+)
