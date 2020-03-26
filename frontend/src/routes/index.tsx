3a72f1d3ca6a6c06b71b64132b1c1b3add5f895a
rodrigo novais <roo.novais@gmail.com>
2020-03-26 19:31:33 -0300

    Profile page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Login from '../pages/Login'
+import Profile from '../pages/Profile'
 import Register from '../pages/Register'
 
 export default () => (
@@ -9,6 +10,7 @@ export default () => (
         <Switch>
             <Route path="/" exact component={Login} />
             <Route path="/register" component={Register} />
+            <Route path="/profile" component={Profile} />
         </Switch>
     </BrowserRouter>
 )
