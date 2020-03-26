0acc7893838252c26c1984f1353c9af02a16032a
rodrigo novais <roo.novais@gmail.com>
2020-03-26 19:49:11 -0300

    New Incident page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Login from '../pages/Login'
 import Profile from '../pages/Profile'
 import Register from '../pages/Register'
+import NewIncident from '../pages/NewIncident'
 
 export default () => (
     <BrowserRouter>
@@ -11,6 +12,7 @@ export default () => (
             <Route path="/" exact component={Login} />
             <Route path="/register" component={Register} />
             <Route path="/profile" component={Profile} />
+            <Route path="/incidents/new" component={NewIncident} />
         </Switch>
     </BrowserRouter>
 )
